2f964c21cd68eb81a2b08c8cb4f151a1a6a503f2
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-10-04 12:39:29 +0530

    Sidebar done

---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[18%] min-h-screen border-r-2'>
+        <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to='/admin/add-business'>
+                <img className='w-5 h-5' src={assets.add_icon} alt="" />
+                <p className='hidden md:block'>Add Business</p>
+            </NavLink>
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to='/admin/businesses'>
+                <img className='w-5 h-5' src={assets.order_icon} alt="" />
+                <p className='hidden md:block'>List businesses</p>
+            </NavLink>
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to='/admin/add-user'>
+                <img className='w-5 h-5' src={assets.profile_icon} alt="" />
+                <p className='hidden md:block'>Add User</p>
+            </NavLink>
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to='/admin/users'>
+                <img className='w-5 h-5' src={assets.order_icon} alt="" />
+                <p className='hidden md:block'>List users</p>
+            </NavLink>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
